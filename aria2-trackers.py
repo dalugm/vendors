dc04cd00bfe9de1cd25779ea3df9a6377e234c6c
dalu98 <moutong945@outlook.com>
2020-04-03 14:29:14 +0800

    Update aria2 trackers by this file

---
+++ aria2-trackers.py
@@ -0,0 +1,46 @@
+import re
+import os
+import requests
+
+def get_html(url):
+    """
+    get html text
+    """
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = 'utf-8'
+        return r.text
+    except:
+        print("connection timed out")
+        return ""
+
+def change_trackers(text):
+    """
+    add website to ~/.aria2/aria2.conf
+    """
+    s = text.split('\n')
+    string = "bt-tracker="
+    string_pattern = "bt-tracker=.*"
+
+    home = os.environ['HOME']
+
+    file = open(home+'/.aria2/aria2.conf', "r")
+    alllines = file.readlines()
+    file.close()
+
+    file = open(home+'/.aria2/aria2.conf', "w")
+
+    while '' in s:
+        s.remove('')
+    string += ",".join(s)
+
+    for line in alllines:
+        line_replace = re.sub(string_pattern, string, line)
+        file.write(line_replace)
+
+    file.close()
+
+URL = 'https://ngosang.github.io/trackerslist/trackers_best.txt'
+TEXT = get_html(URL)
+change_trackers(TEXT)
