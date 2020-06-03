1d8d8dd4fb2aaffd3761b454de5c9bff218e9ae3
dalu98 <moutong945@outlook.com>
2020-06-03 21:13:16 +0800

    open tmux quickly

--- shell/open-tmux.sh
+++ shell/open-tmux.sh
@@ -1,33 +1,40 @@
-#!/usr/bin/env sh
-##################################################
-# name     : tmuxen， tmux environment made easy
-# author   : Xu Xiaodong xxdlhy@gmail.com
-# license  : GPL
-# created  : 2012 Jul 01
-# modified : May 02, 2020
-##################################################
+# !/usr/bin/env bash
+# Filename: open-tmux.sh
+# Author: Xu Xiaodong <xxdlhy@gmail.com>
+# Maintainer: dalu <mou.tong@qq.com>
+# Created: 2012 Jul 01
+# Last Upated: 2020-06-03 18:27
+#          By: dalu <mou.tong@qq.com>
+# Keywords: tmux
+# Version: 0.1
+# Changelog:
+#     0.1 - initial version
+#     0.2 - modified
+# Commentary:
 
-cmd=$(which tmux) # tmux path
-session=daily   # session name
+# open tmux quickly
+
+# Code:
+
+cmd=$(which tmux)      # tmux path
+session=$(hostname -s) # session name
 
 if [ -z $cmd ]; then
-  echo "Tmux not installed."
-  exit 1
+    echo "Tmux not installed."
+    exit 1
 fi
 
-$cmd has -t $session
+$cmd has -t $session 2> /dev/null
 
-if [ $? != 0 ]; then
-  $cmd new -d -n daily -s $session "daily"
-  $cmd splitw -v -p 20 -t $session "edit"
-  $cmd neww -n edit -t $session "edit"
-  $cmd neww -n program -t $session "program"
-  $cmd neww -n update -t $session "update"
-  $cmd neww -n aria2 -t $session "aria2"
-  $cmd splitw -h -p 50 -t $session "zsh"
-  $cmd selectw -t $session:5
+if [ $? -ne 0 ]; then
+    $cmd new-session -d -n daily   -s $session
+    $cmd new-window  -d -n edit    -t $session:2
+    $cmd new-window  -d -n program -t $session:3
+    $cmd new-window  -d -n update  -t $session:4
+    $cmd new-window  -d -n aria2   -t $session:6
+    $cmd new-window  -d -n zsh     -t $session:5
 fi
 
-$cmd att -t $session
+$cmd attach-session -t $session
 
 exit 0
