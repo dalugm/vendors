4386f3562c86268b0c591c13bc866b77e901ca8c
dalu <moutong945@outlook.com>
2021-03-06 09:57:30 +0800

    Update

--- c-cpp/quote_trans.c
+++ c-cpp/quote_trans.c
@@ -2,11 +2,6 @@
 
 int main()
 {
-#ifdef LOCAL
-    freopen("test.in", "r", stdin);
-    freopen("test.out", "w", stdout);
-#endif
-
     int c, q = 1;
     while ((c = getchar()) != EOF) {
         if(c == '"') { printf("%s", q ? "“" : "”"); q = !q; }
